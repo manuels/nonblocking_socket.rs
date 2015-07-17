ee8b2f96b567733164b1ab9f19e5c44ae4587368
Manuel Schölling <manuel.schoelling@gmx.de>
2015-07-17 23:12:34 +0200

    Add set_blocking() function

--- src/lib.rs
+++ src/lib.rs
@@ -14,12 +14,40 @@ use std::thread;
 #[cfg(test)]
 use std::sync::mpsc::channel;
 
+const O_NONBLOCK: c_int = 00004000;
+const F_GETFL: c_int = 3;
+const F_SETFL: c_int = 4;
+
+mod syscall {
+	use libc::c_int;
+
+	extern "C" {
+		pub fn fcntl(fd: c_int, cmd: c_int, flags: c_int) -> c_int;
+	}
+}
+
+pub fn set_blocking(fd: c_int, blocking: bool) -> Result<()> {
+	let flags = unsafe { syscall::fcntl(fd, F_GETFL, 0) };
+	if flags < 0 {
+		return Err(Error::last_os_error());
+	}
+
+	let flags = if blocking { flags & !O_NONBLOCK } else { flags|O_NONBLOCK };
+	let res = unsafe { syscall::fcntl(fd, F_SETFL, flags) };
+	if res != 0 {
+		return Err(Error::last_os_error());
+	}
+
+	Ok(())
+}
+
 extern "C" {
 	fn ioctl(fd: c_int, req: c_ulong, res: *mut c_int) -> c_int;
 }
 
-struct NonBlockingSocket<T:AsRawFd> {
+pub struct NonBlockingSocket<T:AsRawFd> {
 	sock: T,
+	blocking: bool,
 }
 
 const FIONREAD: c_ulong = 0x541B;
@@ -27,8 +55,17 @@ const FIONREAD: c_ulong = 0x541B;
 impl<T:AsRawFd+Read+Write> NonBlockingSocket<T> {
 	pub fn new(sock: T) -> NonBlockingSocket<T> {
 		NonBlockingSocket {
-			sock: sock
+			sock: sock,
+			blocking: true,
 		}
+	}
+
+	pub fn set_blocking(&mut self, blocking: bool) -> Result<()> {
+		let fd = self.sock.as_raw_fd();
+		try!(set_blocking(fd, blocking));
+		self.blocking = blocking;
+
+		Ok(())
 	}
 
 	pub fn pending(&self) -> Result<usize> {
@@ -52,7 +89,7 @@ impl<T:AsRawFd+Read+Write> NonBlockingSocket<T> {
 
 impl<T:AsRawFd+Read+Write> Read for NonBlockingSocket<T> {
 	fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
-		if try!(self.pending()) > 0 {
+		if try!(self.pending()) > 0 || self.blocking {
 			self.sock.read(buf)
 		} else {
 			Ok(0)
@@ -65,9 +102,9 @@ impl<T:AsRawFd+Read+Write> Write for NonBlockingSocket<T> {
 		self.sock.write(buf)
 	}
 
-    fn flush(&mut self) -> Result<()> {
-    	self.sock.flush()
-    }
+	fn flush(&mut self) -> Result<()> {
+		self.sock.flush()
+	}
 }
 
 #[test]
